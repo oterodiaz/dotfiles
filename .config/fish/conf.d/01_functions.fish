983a89093bffc9b9ccbd2f26e11012dd55cce4cb
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2022-06-25 17:53:52 +0200

    Make the colorscheme function available to scripts

--- .config/fish/conf.d/01_functions.fish
+++ .config/fish/conf.d/01_functions.fish
@@ -1,3 +1,41 @@
+function colorscheme
+    function usage
+        printf 'Usage: colorscheme [-h] [-l] [COLORSCHEME]\n\n' 1>&2
+        printf 'Options:\n' 1>&2
+        printf -- '-h\tShow this message\n' 1>&2
+        printf -- '-l\tShow all colorscheme names\n' 1>&2
+    end
+
+    if not test -d "$__fish_config_dir/colorschemes"
+        mkdir -p "$__fish_config_dir/colorschemes"
+    end
+
+    if test (count $argv) -ne 1
+        usage
+        return 1
+    end
+
+    if test "$argv[1]" = '-l'
+        if ! find "$__fish_config_dir/colorschemes" -name '*.fish' -type f | grep -q .
+            printf '%sYou don\'t have any colorschemes installed%s\n' (set_color -o red) (set_color normal)
+            printf 'You can install them by putting *.fish files inside %s or a subfolder\n' "\$__fish_config_dir/colorschemes"
+        else
+            find "$__fish_config_dir/colorschemes" -name '*.fish' -type f -exec basename '{}' \; | sed 's/\.fish//'
+        end
+    else if test "$argv[1]" = '-h'
+        usage 2>&1
+    else
+        if find "$__fish_config_dir/colorschemes" -name "$argv[1].fish" -type f | grep -q .
+            source (find "$__fish_config_dir/colorschemes" -name "$argv[1].fish" -type f)
+            printf 'Applied colorscheme: %s\n' "$argv[1]"
+        else
+            printf "The colorscheme '%s' doesn't exist\n" "$argv[1]" 1>&2
+            return 1
+        end
+    end
+    functions -e usage
+end
+
 if not status --is-interactive
     return
 end
@@ -44,44 +82,6 @@ if command -v zsh &> /dev/null
         set -f ZDOTDIR (zsh -c 'echo "$ZDOTDIR"')
     end
     alias zcd="cd $ZDOTDIR"
-end
-
-function colorscheme
-    function usage
-        printf 'Usage: colorscheme [-h] [-l] [COLORSCHEME]\n\n' 1>&2
-        printf 'Options:\n' 1>&2
-        printf -- '-h\tShow this message\n' 1>&2
-        printf -- '-l\tShow all colorscheme names\n' 1>&2
-    end
-
-    if not test -d "$__fish_config_dir/colorschemes"
-        mkdir -p "$__fish_config_dir/colorschemes"
-    end
-
-    if test (count $argv) -ne 1
-        usage
-        return 1
-    end
-
-    if test "$argv[1]" = '-l'
-        if ! find "$__fish_config_dir/colorschemes" -name '*.fish' -type f | grep -q .
-            printf '%sYou don\'t have any colorschemes installed%s\n' (set_color -o red) (set_color normal)
-            printf 'You can install them by putting *.fish files inside %s or a subfolder\n' "\$__fish_config_dir/colorschemes"
-        else
-            find "$__fish_config_dir/colorschemes" -name '*.fish' -type f -exec basename '{}' \; | sed 's/\.fish//'
-        end
-    else if test "$argv[1]" = '-h'
-        usage 2>&1
-    else
-        if find "$__fish_config_dir/colorschemes" -name "$argv[1].fish" -type f | grep -q .
-            source (find "$__fish_config_dir/colorschemes" -name "$argv[1].fish" -type f)
-            printf 'Applied colorscheme: %s\n' "$argv[1]"
-        else
-            printf "The colorscheme '%s' doesn't exist\n" "$argv[1]" 1>&2
-            return 1
-        end
-    end
-    functions -e usage
 end
 
 if not test -f "$__fish_config_dir/fish_plugins"
