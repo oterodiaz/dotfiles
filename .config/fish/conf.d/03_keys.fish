49d0e4ea32ff1d5f16fbba8aa5dc4171fb24e50b
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2023-01-10 00:27:10 +0100

    Map C-r to history-pager

--- .config/fish/conf.d/03_keys.fish
+++ .config/fish/conf.d/03_keys.fish
@@ -3,8 +3,14 @@ if not status --is-interactive
 end
 
 if test "$fish_key_bindings" = 'fish_vi_key_bindings'
+    bind -M insert \cr history-pager
+    bind -M default \cr history-pager
+
     bind -M insert \cf forward-char
-    bind -M insert \cs forward-char
     bind -M default \cf forward-char
+
+    bind -M insert \cs forward-char
+    bind -M default \cs forward-char
+
     bind -M default \x20 edit_command_buffer
 end
